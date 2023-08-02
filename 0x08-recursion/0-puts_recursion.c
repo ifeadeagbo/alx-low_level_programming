e68c8c890b6fc090fd60eaeacfda7f1b9162cd0a
ifeadeagbo <isadeagbo04@gmail.com>
2023-08-02 15:34:27 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
+/*
+ * _puts_recursion(char *s) - print the strings
+ */
+
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
+		_putchar('\n');
+	}	
+	_putchar(*s);
+	_puts_recursive(s + 1);
+	
 }
