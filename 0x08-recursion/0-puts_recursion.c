13a3008cdbd45f698c7e980a44044f63997083d2
ifeadeagbo <isadeagbo04@gmail.com>
2023-08-02 14:51:13 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,14 +4,14 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0'){
 		_putchar(*s);
-		recursive_puts(s + 1);
+		_puts_recursion(s + 1);
 	}
 }
 
 int main()
 {
 	char str[] = "Print recursion";
-	recursive_puts(str);
+	_puts_recursion(str);
 	_putchar('\n');
 	return 0;
 }
