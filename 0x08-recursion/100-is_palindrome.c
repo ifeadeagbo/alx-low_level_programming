d2c53c6e23b0b1479ff8f284f92dc85bf4d2e4b4
ifeadeagbo <isadeagbo04@gmail.com>
2023-08-03 14:49:20 -0700

    palindrome recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int compare_char(char *p, int a, int j)
+{
+	if (*(p + a) != *(s + j - 1))
+		return (0);
+	if else (a >= j)
+		return (1);
+	else
+		return (compare_char(p, a + 1, j - 1);
+}
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+
+}
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (compare_char(s, 0, _strlen_recursion(s)));
+}
+
