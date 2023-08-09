4bbdbcf0bf5aa6295e464b3df5033ecc63c95e06
ifeadeagbo <isadeagbo04@gmail.com>
2023-08-08 18:16:03 -0700

    memory allocation

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *str_concat(char *s1, char *s2)
+{
+	char *strcat;
+	unsigned int x, y, z, band;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (x = 0; s1[x] != '\0'; x++);
+
+	for (y = 0; s2[y] != '\0'; y++);
+
+	strcat = malloc(sizeof(char) * (x + y + 1));
+
+	if (strcat == NULL)
+	{
+		free(strcat);
+		return (NULL);
+	}
+
+	for (z = 0; z < x; z++)
+		strcat[z] = s1[z];
+
+	band = y;
+	for (y = 0; y <= band; k++, y++)
+		strcat[z] = s2[y];
+
+	return (strcat);
+}
